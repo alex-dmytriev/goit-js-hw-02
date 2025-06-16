e6d25c149edd47705e37592aa4b49ea40522a6d8
Oleksandr Dmytriev <sanderdsukr@gmail.com>
2025-06-16 12:42:26 +0300

    tasks done

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,15 @@
-function getElementWidth(content, padding, border) {
-    const totalWidth = Number.parseFloat(content) + Number.parseFloat(padding)*2 + Number.parseFloat(border)*2;
-    return totalWidth;
+function checkForSpam(message) {
+    const prettyMessage = String(message).toLowerCase();
+    const spamcheck = prettyMessage.includes('spam') || prettyMessage.includes('sale');
+    return spamcheck;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 20
+console.log('--- Task #3 Result ---');
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,31 @@
-function getElementWidth(content, padding, border) {
-    const totalWidth = Number.parseFloat(content) + Number.parseFloat(padding)*2 + Number.parseFloat(border)*2;
-    return totalWidth;
+function getShippingCost(country) {
+  let price;
+  switch (String(country).toLowerCase()) {
+    case 'china': {
+      price = 100;
+      return `Shipping to ${country} will cost ${price} credits`
+    }
+    case 'chile': {
+      price = 250;
+      return `Shipping to ${country} will cost ${price} credits`
+    }
+    case 'australia': {
+      price = 170;
+      return `Shipping to ${country} will cost ${price} credits`
+    }
+    case 'jamaica': {
+      price = 170;
+      return `Shipping to ${country} will cost ${price} credits`
+    }
+    default: return 'Sorry, there is no delivery to your country'
+  }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 20
+console.log('--- Task #4 Result ---');
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
